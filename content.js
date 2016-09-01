3cba789f37af8491cc4c8d6fc2a40445f20160d0
Minsoo Park <minsoo1003@gmail.com>
2016-09-01 14:49:16 +0900

    Initial commit

---
+++ content.js
@@ -0,0 +1,6 @@
+$("<style type='text/css'> div[id*='div-gpt-ad'] {display: none;} </style>").appendTo("head");
+
+var trashes = ["#comment_wrapper", ".aside_sponser", ".content_hd_rgt"];
+for (var i = 0 ; i < trashes.length ; i++) {
+  $(trashes[i]).hide();
+}
