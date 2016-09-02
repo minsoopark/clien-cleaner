948272a39295f89d139a33cf916bbbd1d9196005
Minsoo Park <minsoo1003@gmail.com>
2016-09-02 14:22:43 +0900

    Add mobile support

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-$("<style type='text/css'> div[id*='div-gpt-ad'] {display: none;} </style>").appendTo("head");
+$("<style type='text/css'> div[id*='div-gpt-ad'], div[class*='reply'] {display: none;} </style>").appendTo("head");
 
 var trashes = ["#comment_wrapper", ".aside_sponser", ".content_hd_rgt"];
 for (var i = 0 ; i < trashes.length ; i++) {
